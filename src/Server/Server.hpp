c5a3b974a5e4ab875cb2c267c556a7a48f9200f2
Wing Lam Chan <wchan@c1r5p19.hive.fi>
2024-03-28 17:45:21 +0200

    working on server

---
+++ src/Server/Server.hpp
@@ -0,0 +1,97 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#define BACKLOG 100
+#define BUFFER_SIZE 1024
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stdexcept>
+#include <sys/poll.h>
+#include <arpa/inet.h>
+#include <csignal>
+#include <fcntl.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+typedef typename std::vector<std::unordered_map<std::string, std::string>> config_t;
+typedef typename std::unordered_map<int, std::unordered_map<std::string, std::string>> server_sockets_t;
+
+class Server
+{
+
+private:
+	Server(Server const &src);
+	Server &operator=(Server const &rhs);
+
+	server_sockets_t server_sockets;
+	std::vector<pollfd> fds;
+
+	void setUpServerSocket();
+	void serverLoop();
+	void acceptNewConnection(pollfd &fd);
+	void recvRequest(std::vector<pollfd>::iterator it);
+	void sendResponse(std::vector<pollfd>::iterator it);
+
+	// to be replaced by config file
+	config_t servers;
+
+	struct
+	{
+		struct sockaddr_in address;
+		socklen_t addrlen;
+	} client;
+
+public:
+	class SocketCreationException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class SocketSetNonBlockingException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class SocketBindingException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class SocketListenException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class SocketSetOptionException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class PollException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class AcceptException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	Server();
+	~Server();
+
+	void runServer();
+};
+
+#endif
