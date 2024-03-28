c673b128010e2919670e845958f73b956aed9874
Valeria Vagapova <lera.vagapova@gmail.com>
2024-03-28 18:35:19 +0200

    add a few method prototypes and cmments

--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -1,10 +1,20 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
+#include <string.h>
+#include <vector>
+
 class Request
 {
 private:
 public:
+	// Constructor that takes a string representing start-line and header file lines
+	Request(const std::vector<char> &str);
+	~Request();
+
+	void setBody(const std::vector<char> &str) const;
+	// Also add getters for all the info - or maybe a single getter?
+	// no it's a stupid idea because I would need to define a struct for that...
 };
 
 #endif
--- src/defines.hpp
+++ src/defines.hpp
@@ -2,5 +2,7 @@
 #define DEFINES_HPP
 
 #define CRLF "\r\n"
+#define SP " "
+#define DEFAULT_TIMEOUT 1000
 
 #endif
