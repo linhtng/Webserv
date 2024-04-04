2189d4b597ae619f9b5661140a65ab8bc15ca107
Valeria Vagapova <lera.vagapova@gmail.com>
2024-04-04 17:21:34 +0300

    add summy response

--- src/Response/Response.hpp
+++ src/Response/Response.hpp
@@ -1,10 +1,20 @@
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
 
+#include "../defines.hpp"
+#include "../Request/Request.hpp"
+
 class Response
 {
 private:
+	Response(){};
+	std::vector<std::byte> _body;
+
 public:
+	Response(const Request &request);
+	~Response(){};
+	std::string getHeader() const;
+	std::vector<std::byte> getBody() const;
 };
 
 #endif
