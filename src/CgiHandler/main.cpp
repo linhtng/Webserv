323dd55aee5afd17059bd2a2093afb5d405faaae
linh nguyen <thuynguy@student.hive.fi>
2024-05-01 23:05:07 +0300

    config location: trim slash in directive values for routes

--- src/CgiHandler/main.cpp
+++ src/CgiHandler/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char **argv)
         ConfigParser parser(fileName);
         parser.extractServerConfigs();
         std::vector<ConfigData> servers = parser.getServerConfigs();
+        // parser.printCluster();
         ConfigData server = servers[0];
 
         Request request(server, "GET /time.py HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\r\nAccept: */*\r\nConnection: close\r\nContent-Length: 100\r\nContent-Type: multipart/form-data");
